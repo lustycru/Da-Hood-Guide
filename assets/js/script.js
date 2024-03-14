531163d3e29160fffea9de00f2ca4ec9e4b4a691
Jules NEVEU <jneveu@edenschool.fr>
2024-03-14 17:54:56 +0100

    burger menu  finish

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const sideNav = document.querySelector('.my-side-nav');
+const openBtn = document.querySelector('.fa-bars');
+const closeBtn = document.querySelector('.close');
+openBtn.addEventListener('click', function(e){
+    sideNav.classList.add('active')
+})
+closeBtn.addEventListener('click', function(e){
+    sideNav.classList.remove('active')
+})
