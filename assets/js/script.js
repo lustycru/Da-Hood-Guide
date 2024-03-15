44d376ef8c68bb28566d2b40a3218e002f97a788
Jules NEVEU <jneveu@edenschool.fr>
2024-03-15 12:19:05 +0100

    Home presque fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const sideNav = document.querySelector('.my-side-nav');
 const openBtn = document.querySelector('.fa-bars');
 const closeBtn = document.querySelector('.close');
+
 openBtn.addEventListener('click', function(e){
     sideNav.classList.add('active')
 })
