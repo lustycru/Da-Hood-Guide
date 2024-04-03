4e8030fb08e932207fe877c0c4423e2fa2e41bc8
Jules NEVEU <jneveu@edenschool.fr>
2024-04-03 10:08:21 +0200

    Deuxieme page fini
    Deuxieme page fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ const sideNav = document.querySelector('.my-side-nav');
 const openBtn = document.querySelector('.fa-bars');
 const closeBtn = document.querySelector('.close');
 
+const frenchFlag = document.querySelector('.french-flag')
+const usaFlag = document.querySelector('.usa-flag')
+
 openBtn.addEventListener('click', function(e){
     sideNav.classList.add('active')
 })
